5fb51061717fc44535d33154846784828b7d6b4c
Juttiz <juttiz1992@gmail.com>
2022-06-20 15:34:50 +0800

    1st commit 220620

---
+++ crawler_220616.rb
@@ -0,0 +1,13 @@
+#nokogiri
+
+require "nokogiri"
+require "open-uri"
+
+pageurl = "https://www.books.com.tw/web/sys_saletopb/books"
+page = Nokogiri::HTML(URI.open(pageurl))
+
+
+books = page.css(".item h4")
+books.each do |book|
+    puts book.text
+end
